6a03dd8015c39e179ff51e84c6bb599030a0a28a
Peter Alcock <peteralcock@users.noreply.github.com>
2024-08-18 15:35:04 -0400

    Create __init__.py

---
+++ craigslistscraper/__init__.py
@@ -0,0 +1,13 @@
+from .ad import Ad
+from .ad import AdParser
+from .ad import fetch_ad
+
+from .search import Search
+from .search import SearchParser
+from .search import fetch_search 
+
+from .utils import get_us_cities
+
+__version__ = "1.1.2"
+
+
